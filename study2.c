7f2ecb9c14b6fd2c9f882be2e418570daefe18ac
10119-JYC-Mario-O-O <toadchen83@gmail.com>
2024-05-06 20:00:11 +0900

    add study2

---
+++ study2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main () {
+    int num;
+
+    printf("Write Number : ");
+
+    scanf("%d", &num);
+
+    printf("%d", num);
+
+    printf("\nHello World\n");
+
+    return 0;
+}
