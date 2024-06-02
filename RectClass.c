86e9dad0e4435d8a7afd738c9478ad67368e4303
iltcuhtml <118600849+iltcuhtml@users.noreply.github.com>
2024-06-02 15:20:10 +0900

    Update RectClass.c

--- RectClass.c
+++ RectClass.c
@@ -7,16 +7,19 @@
 
 int main(void)
 {
-    Rect *Rect1 = newRect(4, 5);
-    Rect *Rect2 = newRect(7, 15);
+    Rect Rect1 = newRect(4, 5);
+    Rect Rect2 = newRect(7, 10);
+    Rect Rect3 = newRect(10, 5);
 
-    Rect1->draw(Rect1->this);
-    Rect2->draw(Rect2->this);
+    Rect1.draw(Rect1);
+    Rect2.draw(Rect2);
+    Rect3.draw(Rect3);
 
     deleteRect(Rect1);
     deleteRect(Rect2);
+    deleteRect(Rect3);
 
     getch();
-
+    
     return 0;
 }
