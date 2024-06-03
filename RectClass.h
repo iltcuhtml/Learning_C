9f95ec13502771fefcc966acb48ab1aab51f2bdd
iltcuhtml <118600849+iltcuhtml@users.noreply.github.com>
2024-06-03 12:59:45 +0900

    Update RectClass.h

--- RectClass.h
+++ RectClass.h
@@ -1,34 +1,44 @@
 #ifndef _RectClass_h_
 #define _RectClass_h_
-    typedef struct Rect
-    {
+    typedef struct Rect {
         unsigned int width, height;
 
         void (*draw)();
-    }Rect;
+        void (*delete)();
+    } Rect;
 
     Rect newRect(unsigned int width, unsigned int height);
-    void draw_(const Rect obj);
-    void deleteRect(Rect obj_ptr);
+    void drawRect(Rect rect);
+    void deleteRect(Rect rect);
 
-    Rect newRect(unsigned int width, unsigned int height)
-    {
-        Rect *temp = malloc(sizeof(Rect));
+    Rect newRect(unsigned int width, unsigned int height) {
+        Rect *rect = malloc(sizeof(Rect));
 
-        temp->width = width;
-        temp->height = height;
+        if (rect == NULL) {
+            printf("malloc() failed\n");
 
-        temp->draw = draw_;
+            exit(1);
+        }
 
-        return *temp;
+        rect->width = width;
+        rect->height = height;
+
+        rect->draw = drawRect;
+        rect->delete = deleteRect;
+
+        return *rect;
     }
 
-    void draw_(const Rect obj)
-    {
-        for(int y = 0; y < obj.height; y++)
-        {
-            for (int x = 0; x < obj.width; x++)
-            {
+    void drawRect(Rect rect) {
+        // if (rect == NULL)
+        // {
+        //     printf("Rect is NULL\n");
+
+        //     return;
+        // }
+
+        for (int y = 0; y < rect.height; y++) {
+            for (int x = 0; x < rect.width; x++) {
                 printf("#");
             }
 
@@ -40,10 +50,15 @@
         return;
     }
 
-    void deleteRect(Rect obj_ptr)
-    {
-        free(&obj_ptr);
+    void deleteRect(Rect rect) {
+        // if (rect == NULL)
+        // {
+        //     printf("Rect is NULL\n");
 
-        return;
+        //     return;
+        // }
+
+        free(&rect);
+    
     }
 #endif
