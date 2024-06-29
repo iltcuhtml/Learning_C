f7a495754a8b4cee972079daef78342138352b24
iltcuhtml <118600849+iltcuhtml@users.noreply.github.com>
2024-06-30 01:27:34 +0900

    PI.c

---
+++ PI.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <math.h>
+
+#include <conio.h>
+
+int main(void)
+{
+    unsigned int n = 1;
+    double value = 0;
+
+    for (;;)
+    {
+        value += 1 / pow(n, 2);
+        n++;
+
+        PI = sqrt(value * 6);
+
+        printf("n : %u", n);
+        printf("PI : %lf", PI);
+        printf("차이 : %lf", M_PI - PI);
+    }
+
+    getch();
+
+    return 0;
+}
